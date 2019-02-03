ceed626acdb7338be7e43c594003c0787b8ebdac
Niya Panamdanam <niya@findniya.com>
2019-02-02 19:24:38 -0500

    WIP function to handle img from user and the async function to upload im

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import InitialView from './components/InitialView'
 
 export default class App extends React.Component {
-  constructor (props){
+  constructor (props) {
     super(props)
-    this.state={
+    this.state = {
       img: null,
       loading: false,
     }
     this.takePhoto = this.takePhoto.bind(this)
     this.selectPhoto = this.selectPhoto.bind(this)
+    this.handleImgResult = this.handleImgResult.bind(this)
   }
 
   takePhoto () {
@@ -18,8 +19,41 @@ export default class App extends React.Component {
   }
 
   selectPhoto () {
-    console.log('yep')
+    //getting user permission for gallery
+    const {
+      status: cameraRollPermit
+    } = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+
+    if (cameraRollPermit === 'granted') {
+      let imgResult = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 3]
+      })
+    }
+    this.handleImgResult(imgResult)
   }
+
+  //fn to update img state when user picks an img
+  handleImgResult = async (imgResult) => {
+    try {
+      this.setState({
+        loading: true
+      })
+      if (!imgResult.cancelled) {
+        this.setState({
+          img: imgResult
+        })
+      }
+    } catch (e) {
+      console.log({e})
+      alert('Failed Image Upload :(')
+    } finally {
+      this.setState({
+        loading: false
+      })
+    }
+  }
+
 
   render () {
     return (
@@ -33,7 +67,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EDEEF7',
     flexDirection: 'column'
   },
 })
